847df6f5c45ebd6bda50710ed6c3f6c1b86478df
Carlo Pearson <cpearson@webmdhealth.net>
2016-05-20 16:28:00 -0700

    updated test

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -1,20 +1,28 @@
 'use strict';
 
 import {expect} from 'chai';
+import {List} from 'immutable';
 
-describe('immutablility', () => {
-	describe('a number', () => {
-		
-		function increment(currentState) {
-			return currentState + 1;
-		}
+describe('A List', () => {
 
-		it('is immutable', () => {
-			let state = 42;
-			let nextState = increment(state);
+	function addMovie(currentState, movie) {
+		return currentState.push(movie);
+	}
 
-			expect(nextState).to.equal(43);
-			expect(state).to.equal(42);
-		});
+	it('is immutable', () => {
+		let state = List.of('Trainspotting', '28 Days Later');
+		let nextState = addMovie(state, 'Sunshine');
+
+		expect(nextState).to.equal(List.of(
+			'Trainspotting',
+			'28 Days Later',
+			'Sunshine'
+		));
+
+		expect(state).to.equal(List.of(
+			'Trainspotting',
+			'28 Days Later'
+		));
 	});
+
 });
